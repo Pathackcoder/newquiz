c3a75bc2247c871765f4f4e4c89a308d38aebc7e
Amit Pathak <91179150+Pathackcoder@users.noreply.github.com>
2024-05-19 14:26:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+const quizData = [
+    {
+        question: "What's Akash favourite Movie",
+        options: ["The Notebook", "Titanic", "Avengers: End Game", "La La Land"],
+        answer: ""
+    },
+    {
+        question: "What's Akash idea of a perfect date?",
+        options: ["Dinner at a fancy restaurant", "Picnic under the stars", "Watching sunset at Fatehpur Sikri", "Stargazing on a rooftop"],
+        answer: ""
+    },
+    {
+        question: "Which love song Akash like?",
+        options: ["Satranga", "Pehle bhi main", "Perfect", "Zaalima"],
+        answer: ""
+    },
+];
+
+const quizContainer = document.getElementById("quiz");
+const questionElement = document.getElementById("question");
+const optionsElement = document.getElementById("options");
+const submitButton = document.getElementById("submit");
+const resultElement = document.getElementById("result");
+
+let currentQuestion = 0;
+
+function loadQuestion() {
+    const currentQuizData = quizData[currentQuestion];
+    questionElement.innerText = currentQuizData.question;
+    optionsElement.innerHTML = "";
+    currentQuizData.options.forEach((option, index) => {
+        const button = document.createElement("button");
+        button.innerText = option;
+        button.classList.add("option-button");
+        button.addEventListener("click", () => {
+            currentQuizData.answer = option;
+            selectOption();
+        });
+        optionsElement.appendChild(button);
+    });
+}
+
+function selectOption() {
+    const selectedButton = document.querySelector(".option-button.selected");
+    if (selectedButton) {
+        selectedButton.classList.remove("selected");
+    }
+    const clickedButton = event.target;
+    clickedButton.classList.add("selected");
+}
+
+
+function showResult() {
+    let score = 0;
+    quizData.forEach((question) => {
+        if (question.answer !== "") {
+            score++;
+        }
+    });
+    resultElement.innerText = `Congratulations! Accuracy: 100%`;
+}
+
+function nextQuestion() {
+    if (currentQuestion < quizData.length - 1) {
+        currentQuestion++;
+        loadQuestion();
+    } else {
+        submitButton.style.display = "none";
+        showResult();
+    }
+}
+
+submitButton.addEventListener("click", nextQuestion);
+
+loadQuestion();
